e11ebd1e53d9b07e4e144bc6785b3cc76d7e3dc9
renatodru <renatodruregis@gmail.com>
2022-11-16 04:56:24 -0300

    arquivos deploy

---
+++ docker.img.deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# docker build ./src/emailservice -t emailservice
+# docker build ./src/productcatalogservice -t productcatalogservice
+# docker build ./src/recommendationservice -t recommendationservice
+# docker build ./src/shippingservice -t shippingservice
+# docker build ./src/checkoutservice -t checkoutservice
+# docker build ./src/paymentservice -t paymentservice
+# docker build ./src/currencyservice -t currencyservice
+# docker build ./src/cartservice/src -t cartservice
+docker build ./src/frontend -t frontend
+# docker build ./src/adservice -t adservice
+# docker build ./src/loadgenerator -t loadgenerator
---
+++ kubectl.delete.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# minikube kubectl -- delete -f ./kubernetes-manifests/adservice.yaml
+# minikube kubectl -- delete -f ./kubernetes-manifests/cartservice.yaml
+# minikube kubectl -- delete -f ./kubernetes-manifests/checkoutservice.yaml
+# minikube kubectl -- delete -f ./kubernetes-manifests/currencyservice.yaml
+# minikube kubectl -- delete -f ./kubernetes-manifests/emailservice.yaml
+minikube kubectl -- delete -f ./kubernetes-manifests/frontend.yaml
+# minikube kubectl -- delete -f ./kubernetes-manifests/paymentservice.yaml
+# minikube kubectl -- delete -f ./kubernetes-manifests/productcatalogservice.yaml
+# minikube kubectl -- delete -f ./kubernetes-manifests/recommendationservice.yaml
+# minikube kubectl -- delete -f ./kubernetes-manifests/shippingservice.yaml
+# minikube kubectl -- delete -f ./kubernetes-manifests/loadgenerator.yaml
